d8f97b06829e79947d7c0f6750bf5e3638200e06
ShivamSoam99 <shivamvicioussoam@gmail.com>
2023-08-02 16:42:20 +0200

    databse schema updated

---
+++ java-api/src/main/java/com/db/grad/javaapi/model/User.java
@@ -0,0 +1,57 @@
+package com.db.grad.javaapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="users")
+public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    private long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "password" , nullable = false)
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
