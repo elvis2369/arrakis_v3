76fb67a4d81983d9908fd4dc4551862f01d65f3c
Mircea <mircea_pantazi@yahoo.com>
2023-08-03 12:54:22 +0300

    pusehd from java

--- java-api/src/main/java/com/db/grad/javaapi/repository/SecurityConfig.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/SecurityConfig.java
@@ -11,11 +11,26 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public UserDetailsService userDetailsService() {
         return new UserService();
@@ -47,6 +62,7 @@ public class SecurityConfig {
                 .formLogin().permitAll().defaultSuccessUrl("/home.html")
                 .and()
                 .exceptionHandling().accessDeniedPage("/403");
+                http.cors(cors->cors.disable());
         return http.build();
     }
 }
