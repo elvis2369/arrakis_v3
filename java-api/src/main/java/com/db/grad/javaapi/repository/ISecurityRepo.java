30b0b311364ef58c4ff8149151a968b6c841ce18
Eduard-Costin Dumistracel <eduard_costin2001@yahoo.com>
2023-08-04 15:38:11 +0300

    bondSecurity
    Signed-off-by: Eduard-Costin Dumistracel <eduard_costin2001@yahoo.com>

--- java-api/src/main/java/com/db/grad/javaapi/repository/ISecurityRepo.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/ISecurityRepo.java
@@ -3,6 +3,10 @@ package com.db.grad.javaapi.repository;
 import com.db.grad.javaapi.model.Security;
 import com.db.grad.javaapi.service.SecurityService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface ISecurityRepo extends JpaRepository<Security,Long> {
+    @Query(nativeQuery = true, value = "select * from security where id=:id")
+    public Security getSecurityById(long id);
 }
--- java-api/src/main/java/com/db/grad/javaapi/service/SecurityService.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/SecurityService.java
@@ -4,6 +4,7 @@ import com.db.grad.javaapi.model.Security;
 import com.db.grad.javaapi.repository.ISecurityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 
@@ -16,5 +17,9 @@ public class SecurityService {
     {
         ArrayList<Security> allBonds = (ArrayList<Security>) securityRepo.findAll();
         return allBonds;
+    }
+
+    public Security getSecurityById(long id) {
+        return this.securityRepo.getSecurityById(id);
     }
 }
