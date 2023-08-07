cc6b573077987b00b39ba95c67b6a2428c9cba57
Eduard-Costin Dumistracel <eduard_costin2001@yahoo.com>
2023-08-07 12:59:02 +0300

    add id
    Signed-off-by: Eduard-Costin Dumistracel <eduard_costin2001@yahoo.com>

--- java-api/src/main/java/com/db/grad/javaapi/repository/ISecurityRepo.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/ISecurityRepo.java
@@ -17,8 +17,7 @@ public interface ISecurityRepo extends JpaRepository<Security,Long> {
             "ON trade.security_id = security.id\n" +
             "WHERE trade.book_id IN (SELECT book_id\n" +
             "                   FROM book_user\n" +
-            "                  WHERE user_id=1);")
-      //          "                  WHERE user_id=:id);")
+            "                  WHERE user_id=:id);")
     ArrayList<Security> getMyBonds(long id);
 
     @Query(nativeQuery = true, value = "select * from security where id=:id")
